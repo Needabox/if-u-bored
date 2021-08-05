7fcf4ce2b4b61f17bb540fccc0732533b5387b7a
RAFLI <rafli1162003@gmail.com>
2021-08-05 12:20:52 +0700

    responsive

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -2,7 +2,7 @@
     <div class="flex items-center justify-center mt-10">
     <div class="container"> 
         <div class="flex justify-center">
-                <div class="bg-white shadow-xl rounded-lg w-2/6">
+                <div class="bg-white shadow-xl rounded-lg w-1/2 md:w-2/6">
                     <ul class="divide-y divide-gray-300" v-for="item in activities" :key="item">
                         <li class="p-4 hover:bg-gray-50 cursor-pointer ">{{ item.activity }}</li>
                     </ul>
